9b01a1f5a9d54d5fef91a2041ac004bed902c0c2
Ken Yin <ken.yin@maiscrm.com>
2020-06-28 18:19:35 +0800

    lessons/notes: use vant component in Vue.js homework

--- lessons/notes/src/components/Notes.vue
+++ lessons/notes/src/components/Notes.vue
@@ -1,19 +1,34 @@
 <template>
   <div id="root">
-      <div id="pageTitle">Notes</div>
+      <van-row id="pageTitle">
+          <van-col span="8"></van-col>
+          <van-col span="8">Notes</van-col>
+          <van-col span="8"></van-col>
+      </van-row>
       <div v-for="(note, index) in notes" :key="index">
           <note :index="index" :content="note.text" :color="note.color" style="margin-top:10px"></note>
       </div>
       <div id="addNoteArea">
-          <table>
+          <table style="margin: auto">
             <tr>
-                <td colspan="4"><textarea v-model="notetext" style="width:400px; height:50px"></textarea></td>
+                <td colspan="4">
+                <van-field
+                    v-model="notetext"
+                    rows="2"
+                    autosize
+                    label="note"
+                    type="textarea"
+                    maxlength="50"
+                    placeholder="请输入 note"
+                    show-word-limit
+                    style="border: 1px solid grey; width: 400px"/>
+                </td>
             </tr>
             <tr>
                 <td><div id="redSelector" @click="setColor('red')"></div></td>
                 <td><div id="blueSelector" @click="setColor('blue')"></div></td>
                 <td><div id="greenSelector" @click="setColor('green')"></div></td>
-                <td><button @click="addNote" id="addID">添加</button></td>
+                <td><van-button type="primary" size="small" @click="addNote">添加</van-button></td>
             </tr>
           </table>
       </div>
@@ -48,7 +63,16 @@ export default {
           this.color = colorVal
       },
       delNote(i) {
-          this.notes.splice(i, 1)
+          this.$dialog.confirm({
+            title: '删除操作',
+            message: '是否确定要删除该 note ?',
+            })
+            .then(() => {
+                this.notes.splice(i, 1)
+            })
+            .catch(() => {
+                return
+            });
       }
   },
   watch: {
@@ -68,7 +92,7 @@ export default {
     #addNoteArea{
         position: fixed;
         bottom: 50px;
-        left: 40%;
+        width: 100%;
     }
     #redSelector{
         width: 30px;
